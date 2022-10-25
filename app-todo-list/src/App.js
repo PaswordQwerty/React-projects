471f9788a812be4b3957c405da8d740228c4c0fc
PaswordQwerty <laifake97@mail.ru>
2022-10-25 15:25:46 +0300

    f3

--- app-todo-list/src/App.js
+++ app-todo-list/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import './components/UI/Button'
+import Button from './components/UI/Button';
+import styles from 'styled-components'
+import { useState } from 'react';
+// npm install --save styled-components
+
+const DivStyle = styles.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 10%;
+  flex-direction: column;
+  align-items: center;
+
+  &.invalid Button{
+    background-color: black;
+    color: white;
+  }
+
+  & h2{
+    color: red;
+  }
+`;
 
 function App() {
+
+  const [isInputValid, setIsInputValid] = useState(true);
+
   return (
     <div className="App">
-      <h2>Hacking</h2>
+      <DivStyle className={!isInputValid && 'invalid'}>
+        <Button onClick={()=>setIsInputValid(!isInputValid)}>Кнопка</Button>
+        <h2>Тест</h2>
+      </DivStyle>
     </div>
   );
 }
---
+++ app-todo-list/src/components/UI/Button.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+    background-color: blue;
+    border: 0px;
+    width: 150px;
+    height: 50px;
+    color: white;
+    font-size: 20px;
+    border-radius: 30px;
+`;
+
+export default Button;
