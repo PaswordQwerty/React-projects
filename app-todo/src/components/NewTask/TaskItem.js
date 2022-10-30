8350a79dc9f10c1e8e41eb22d86fd8bf1b68a7e5
PaswordQwerty <laifake97@mail.ru>
2022-10-30 19:51:00 +0300

    f12

--- app-todo/src/components/NewTask/TaskItem.js
+++ app-todo/src/components/NewTask/TaskItem.js
@@ -1,6 +1,6 @@
 import './TaskItem.scss'
 import { db } from '../../firebase';
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 
 const TaskItem = (props) =>{
 
@@ -8,9 +8,13 @@ const TaskItem = (props) =>{
         await deleteDoc(doc(db, "todos", props.id));
     }
 
+    const completedHandler = async () =>{
+        await updateDoc(doc(db, "todos", props.id),{completed: !props.completed})
+    }
+
     return (
         <div className='Task-Item'>
-            <h3 className='Task-Item-h3'>{props.title}</h3>
+            <h3 className='Task-Item-h3' style={{textDecoration: `${props.completed ? 'line-through' : 'none'}`}} onClick={completedHandler}>{props.title}</h3>
             <button onClick={deleteTaskHandler} className='Task-Item-button'>X</button>
         </div>
     );
